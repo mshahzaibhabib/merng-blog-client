0077a5d8aea3e5106be022052f8c40cd0cb2acb0
mshahzaibhabib <mh.shahzaibhabib@gmail.com>
2020-11-02 23:18:27 +0500

    Fixed React not used bug

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ApolloProvider from './ApolloProvider';
 
-// ReactDOM.render(
-//     <React.StrictMode>
-//         {ApolloProvider}
-//     </React.StrictMode>,
-//     document.getElementById('root')
-// );
+ReactDOM.render(
+    <React.StrictMode>
+        {ApolloProvider}
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-ReactDOM.render(ApolloProvider, document.getElementById('root'));
+// ReactDOM.render(ApolloProvider, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
